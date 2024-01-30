3bba0abbde090f8986ab166dfca644470d9d532c
GebsonSilva <gebsonlima@hotmail.com>
2024-01-29 21:55:42 -0300

    first commit

---
+++ desafio1Ruby.rb
@@ -0,0 +1,22 @@
+#Criar programa simples que de acess a leitores de uma biblioteca - digitando nome, sobrenome e idade.
+
+
+print "DIgite seu nome: "
+
+
+#usuário digite o seu nome e sobre nome aqui.
+
+nome = gets.chomp
+
+print "Digite seu sobrenome: "
+
+sobrenome = gets.chomp
+
+#Aqui ele digita a sua idade.
+
+idade = gets.chomp
+
+#saída final
+
+
+puts "Olá #{nome} #{sobrenome}! Seja bem vindo! Sua idade é #{idade}."
